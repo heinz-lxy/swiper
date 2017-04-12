c85cede182d3f189d736f98349c62da2107fdf7c
Heinz <heinz.lxy@gmail.com>
2017-04-12 21:41:16 +0800

    ‘-’

---
+++ animation.js
@@ -0,0 +1,10 @@
+﻿function animation(){
+	var container = document.getElementById('container')
+	var first = document.querySelectorAll('.slider')[0]
+	first.classList.toggle('closed');
+	setTimeout(function(){
+		container.appendChild(first)
+		first.classList.toggle('closed');
+	},500) //等动画执行完成再操作
+}
+setInterval(animation,2000)  
